61fd0f0e43aa33f0650c138dd12ba57c87584e3f
Livia Brandão <livia.brandao@wavy.global>
2021-02-17 16:34:11 -0300

    Adding the generated spring file

---
+++ Studyin/CRUDPets/src/main/kotlin/com/example/CRUDPets/CrudPetsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.CRUDPets
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrudPetsApplication
+
+fun main(args: Array<String>) {
+	runApplication<CrudPetsApplication>(*args)
+}
---
+++ Studyin/CRUDPets/src/test/kotlin/com/example/CRUDPets/CrudPetsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.CRUDPets
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CrudPetsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
